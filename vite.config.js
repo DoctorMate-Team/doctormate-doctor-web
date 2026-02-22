e4b42748b829b8591f6af5a2e034a3477917840d
keroles adel <kerolesadel297@gmail.com>
2026-02-22 14:21:07 +0200

    fixed deploy 01

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  
   build: {
-    outDir: 'dist' // خليها dist لأن Vercel متوافق مع dist تلقائي
+    outDir: 'dist' 
   }
 })
